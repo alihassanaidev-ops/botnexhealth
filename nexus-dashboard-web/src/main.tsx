329b534720efb4372633fafc5d54e08951272fba
Ali2047 <alikkc4024@gmail.com>
2026-02-22 20:51:07 +0500

    feat: enable dark theme via next-themes ThemeProvider

--- nexus-dashboard-web/src/main.tsx
+++ nexus-dashboard-web/src/main.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
+import { ThemeProvider } from 'next-themes'
 import { router } from './router'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <ThemeProvider attribute="class" defaultTheme="dark">
+      <RouterProvider router={router} />
+    </ThemeProvider>
   </React.StrictMode>,
 )
